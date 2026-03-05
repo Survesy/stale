fcb847c9a755883720332e820172c49cb7552010
th1nkful <9315568+th1nkful@users.noreply.github.com>
2026-03-05 21:02:45 +0000

    Update Homebrew formula for 0.2.3

--- Formula/stale.rb
+++ Formula/stale.rb
@@ -1,28 +1,28 @@
 class Stale < Formula
   desc "CLI tool to run or skip commands based on file content hashes"
   homepage "https://github.com/Survesy/stale"
-  version "0.2.2"
+  version "0.2.3"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/th1nkful/stale/releases/download/0.2.2/stale-x86_64-apple-darwin.tar.gz"
-      sha256 "577a3389c5c088e72228c7110ca5566976c6f40c2de9ba0b9dc9916704c981f3"
+      url "https://github.com/Survesy/stale/releases/download/0.2.3/stale-x86_64-apple-darwin.tar.gz"
+      sha256 "7d1c8196a92e913374a7b65e34a5597a0b3736bb0ad9ff0f2b70fb09b7b89843"
     end
     on_arm do
-      url "https://github.com/th1nkful/stale/releases/download/0.2.2/stale-aarch64-apple-darwin.tar.gz"
-      sha256 "3418dbfb35d9a7d0ff93f535e2d40a85ce8afcc201ad405adf8b68dd0ea13af1"
+      url "https://github.com/Survesy/stale/releases/download/0.2.3/stale-aarch64-apple-darwin.tar.gz"
+      sha256 "31940279a40dbb640a2bd309bc07ed050c886a1db2c29fc884c1fd4a8b25cef8"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/th1nkful/stale/releases/download/0.2.2/stale-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "edaf139a1be97682ccb6b95671e508e1b755bdbdb261c363ec9d54921b52f91c"
+      url "https://github.com/Survesy/stale/releases/download/0.2.3/stale-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "8cfbd79527816d2a6d0435f0f15889c462672fb143fc4de31aaa6679a05d4292"
     end
     on_arm do
-      url "https://github.com/th1nkful/stale/releases/download/0.2.2/stale-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9c01691597c67ff0ac912d35d2a923fcd6f6bc15f10fc0fc154bd51452f2d071"
+      url "https://github.com/Survesy/stale/releases/download/0.2.3/stale-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "46bc444d42cd5a0c4b8ab5476dcb6517d1bec1f4f14d415fd4389671e31224f1"
     end
   end
 
